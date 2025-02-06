7869a0752b93f71a69c4d9592f97c6cb90e2f4a9
zshnrg <ghosanirozan1@gmail.com>
2025-02-06 21:17:59 +0700

    Fix: About page responsiveness

--- src/app/_frames/profileFrame/aboutSection.tsx
+++ src/app/_frames/profileFrame/aboutSection.tsx
@@ -6,11 +6,11 @@ import { buildParagraph } from "@/utils/language/paragraph"
 export default function AboutSection() {
     const { language } = useLanguage()
     return (
-        <div className="flex flex-col w-full py-40 px-52 justify-start gap-12">
-            <h2 className="text-6xl font-bold text-white">{contents.profileFrame.aboutSection.title[language]}</h2>
+        <div className="col-span-2 lg:col-span-1 flex flex-col w-full py-40 px-[calc(100%/6)] justify-start gap-6 md:gap-8 lg:gap-12">
+            <h2 className="text-4xl lg:text-6xl font-bold text-white">{contents.profileFrame.aboutSection.title[language]}</h2>
             <div className="flex flex-col gap-6">
                 { contents.profileFrame.aboutSection.content[language].map((paragraph, index) => (
-                    buildParagraph(paragraph, "text-2xl leading-10 text-white text-justify")
+                    buildParagraph(paragraph, "text-lg md:text-xl lg:text-2xl leading-7 md:leading-8 lg:leading-10 text-white text-justify")
                 ))}
             </div>
         </div>
--- src/app/_frames/profileFrame/profileFrame.tsx
+++ src/app/_frames/profileFrame/profileFrame.tsx
@@ -6,7 +6,7 @@ import EducationSection from "./educationSection";
 export default function ProfileFrame() {
     return (
         <div 
-            className="flex lg:flex-row w-full"
+            className="grid grid-cols-2"
             style={{ backgroundImage: "url('/assets/background/gradient_dark.svg')", backgroundSize: "contain", backgroundPosition: "left", backgroundRepeat: "no-repeat" }}
         >
             <AboutSection />
