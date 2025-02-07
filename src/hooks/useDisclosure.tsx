e7b635680f9476e1100bff16f890039cfe92c718
zshnrg <ghosanirozan1@gmail.com>
2025-02-07 21:21:50 +0700

    Fix: useDisclosure multiple click onOpenChange handling

--- src/hooks/useDisclosure.tsx
+++ src/hooks/useDisclosure.tsx
@@ -2,10 +2,23 @@ import { useState, useCallback } from "react";
 
 export function useDisclosure() {
     const [isOpen, setIsOpen] = useState(false)
+    const [isDisabled, setIsDisabled] = useState(false)
 
     const onOpen = useCallback(() => setIsOpen(true), [])
     const onClose = useCallback(() => setIsOpen(false), [])
-    const onOpenChange = useCallback(() => setIsOpen((prev) => !prev), [])
+    const onOpenChange = useCallback(() => {
+        setIsDisabled((prev) => {
+            console.log(prev)
+            if (prev) return true;
+
+            setIsOpen((prevOpen) => {
+                console.log("Setting to", !prevOpen)
+                return !prevOpen;
+            }); 
+            setTimeout(() => setIsDisabled(false), 500);
+            return true;
+        });
+    }, []);
 
     return { isOpen, onOpen, onClose, onOpenChange }
 }
