8da8f03e4b553dbcc52b70e7ab9c1319614e5085
zshnrg <ghosanirozan1@gmail.com>
2025-02-10 13:16:09 +0700

    Fix: Course modal animated

--- src/app/_frames/profileFrame/modals/courseworkModal.tsx
+++ src/app/_frames/profileFrame/modals/courseworkModal.tsx
@@ -3,6 +3,7 @@ import { useDisclosure } from "@/hooks/useDisclosure";
 import { useState } from "react";
 
 import { Modal } from "@/components/ui/modal";
+import { motion, AnimatePresence } from "framer-motion";
 
 import modals from "@/data/modals.json"
 
@@ -19,22 +20,20 @@ type Course = {
 export default function CourseworkModal({ disclosure } : { disclosure: ReturnType<typeof useDisclosure> }) {
 
     const { language } = useLanguage();
-    const [ activeTags, setActiveTags ] = useState<string[]>([]);
+    const [ activeTags, setActiveTags ] = useState<string | null>(null);
 
     const [activeData, setActiveData] = useState<Course[]>([]);
     const [remainingData, setRemainingData] = useState<Course[]>(modals.courseworkModal.data.course);
 
     const handleTagClick = (tag: string) => {
-        if (activeTags.includes(tag)) {
-            setActiveTags(activeTags.filter(activeTag => activeTag !== tag));
-
-            const filteredData = activeData.filter(course => course.tags.includes(tag));
-            setActiveData(activeData.filter(course => !filteredData.includes(course)));
+        if (activeTags === tag) {
+            setActiveTags(null);
+            setActiveData([]);
+            setRemainingData(modals.courseworkModal.data.course);
         } else {
-            setActiveTags([...activeTags, tag]);
-
-            const filteredData = remainingData.filter(course => course.tags.includes(tag));
-            setActiveData([...activeData, ...filteredData]);
+            setActiveTags(tag);
+            setActiveData(modals.courseworkModal.data.course.filter(course => course.tags.includes(tag)));
+            setRemainingData(modals.courseworkModal.data.course.filter(course => !course.tags.includes(tag)));
         }
     }
     
@@ -49,7 +48,7 @@ export default function CourseworkModal({ disclosure } : { disclosure: ReturnTyp
                 <div className="flex flex-wrap gap-3">
                     {modals.courseworkModal.data.tags.map((tags, index) => (
                             <h4 key={tags.tag} 
-                                className={`p-2 rounded-xl font-bold text-md ${(activeTags.includes(tags.tag) ? tags.color.active : tags.color.bg)} ${tags.color.text} ${tags.color.hover} cursor-pointer`} 
+                                className={`p-2 rounded-xl font-bold text-md ${(activeTags === tags.tag ? tags.color.active : tags.color.bg)} ${tags.color.text} ${tags.color.hover} cursor-pointer`} 
                                 onClick={() => handleTagClick(tags.tag)}
                             >
                                 {tags[language]}
@@ -58,18 +57,32 @@ export default function CourseworkModal({ disclosure } : { disclosure: ReturnTyp
                     )}
                     <hr className="w-full border-gray-100 border-4 m-4 rounded-full" />
 
-                    <div className="flex flex-wrap gap-4">
-                        {activeData.map((course, index) => (
-                            <div key={course.name.id} className="flex flex-col gap-2 p-2 bg-sky-300 font-medium text-sm rounded-xl">
-                                {course.name[language]}
-                            </div>
-                        ))}
-                        {remainingData.map((course, index) => (
-                            <div key={course.name.id} className="flex flex-col gap-2 p-2 bg-neutral-100 font-medium text-sm rounded-xl opacity-50">
-                                {course.name[language]}
-                            </div>
-                        ))}
-                    </div>
+                    <AnimatePresence>
+                        <div className="flex flex-wrap gap-4">
+                            {activeData.map((course, index) => (
+                                <motion.div
+                                    initial={{ opacity: 0, y: 20 }}
+                                    animate={{ opacity: 1, y: 0 }}
+                                    exit={{ opacity: 0, y: 20 }}
+                                    transition={{ duration: 0.5, delay: index * 0.025 }}
+                                    key={course.name.id} 
+                                    className="flex flex-col gap-2 p-2 bg-sky-300 font-medium text-sm rounded-xl">
+                                    {course.name[language]}
+                                </motion.div>
+                            ))}
+                            {remainingData.map((course, index) => (
+                                <motion.div
+                                    initial={{ opacity: 0, y: 20 }}
+                                    animate={{ opacity: 1, y: 0 }}
+                                    exit={{ opacity: 0, y: 20 }}
+                                    transition={{ duration: 0.5, delay: index * 0.025 }} 
+                                    key={course.name.id} 
+                                    className="flex flex-col gap-2 p-2 bg-neutral-100 font-medium text-sm rounded-xl opacity-50">
+                                    {course.name[language]}
+                                </motion.div>
+                            ))}
+                        </div>
+                    </AnimatePresence>
                 </div>
             </div>
         </Modal>
