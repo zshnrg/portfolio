091045136fb06c4f4e1cd915ee990c82008e5bd7
zshnrg <ghosanirozan1@gmail.com>
2025-02-11 00:53:02 +0700

    Fix: useWindowDimensions window is not defined

--- src/hooks/useWindowDimensions.tsx
+++ src/hooks/useWindowDimensions.tsx
@@ -1,20 +1,23 @@
 import { useState, useEffect } from 'react';
 
-function getWindowDimensions() {
-  const { innerWidth: width, innerHeight: height } = window;
-  return {
-    width,
-    height
-  };
-}
-
 export default function useWindowDimensions() {
-  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+  const [windowDimensions, setWindowDimensions] = useState<{
+    width: number | undefined
+    height: number | undefined
+  }>({
+    width: undefined,
+    height: undefined
+  });
 
   useEffect(() => {
     function handleResize() {
-      setWindowDimensions(getWindowDimensions());
+      setWindowDimensions({
+        width: window.innerWidth,
+        height: window.innerHeight
+      });
     }
+
+    handleResize();
 
     window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
